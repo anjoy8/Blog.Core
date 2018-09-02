404d656c1d29af24cf1b5a5368f0c500345cbc07
anjoy8 <3143422472@qq.com>
2018-09-02 09:57:40 +0800

    Add files via upload

---
+++ Blog.Core/AuthHelper/OverWrite/JwtHelper.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.AuthHelper
+{
+    public class JwtHelper
+    {
+        public static string secretKey { get; set; } = "sdfsdfsrty45634kkhllghtdgdfss345t678fs";
+        /// <summary>
+        /// 颁发JWT字符串
+        /// </summary>
+        /// <param name="tokenModel"></param>
+        /// <returns></returns>
+        public static string IssueJWT(TokenModelJWT tokenModel)
+        {
+            var dateTime = DateTime.UtcNow;
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti,tokenModel.Uid.ToString()),//Id
+                new Claim("Role", tokenModel.Role),//角色
+                new Claim(JwtRegisteredClaimNames.Iat,dateTime.ToString(),ClaimValueTypes.Integer64)
+            };
+            //秘钥
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtHelper.secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+          
+            var jwt = new JwtSecurityToken(
+                issuer: "Blog.Core",
+                claims: claims, //声明集合
+                expires: dateTime.AddHours(2),
+                signingCredentials: creds);
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var encodedJwt = jwtHandler.WriteToken(jwt);
+
+            return encodedJwt;
+        }
+
+        /// <summary>
+        /// 解析
+        /// </summary>
+        /// <param name="jwtStr"></param>
+        /// <returns></returns>
+        public static TokenModelJWT SerializeJWT(string jwtStr)
+        {
+            var jwtHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken = jwtHandler.ReadJwtToken(jwtStr);
+            object role = new object(); ;
+            try
+            {
+                jwtToken.Payload.TryGetValue("Role", out role);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            var tm = new TokenModelJWT
+            {
+                Uid = long.Parse(jwtToken.Id),
+                Role = role.ToString(),
+            };
+            return tm;
+        }
+    }
+
+    /// <summary>
+    /// 令牌
+    /// </summary>
+    public class TokenModelJWT
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public long Uid { get; set; }
+        /// <summary>
+        /// 角色
+        /// </summary>
+        public string Role { get; set; }
+     
+    }
+}
---
+++ Blog.Core/AuthHelper/OverWrite/JwtTokenAuth.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Blog.Core.AuthHelper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class JwtTokenAuth
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly RequestDelegate _next;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="next"></param>
+        public JwtTokenAuth(RequestDelegate next)
+        {
+            _next = next;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public Task Invoke(HttpContext httpContext)
+        {
+            //检测是否包含'Authorization'请求头
+            if (!httpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                return _next(httpContext);
+            }
+            var tokenHeader = httpContext.Request.Headers["Authorization"].ToString();
+
+            TokenModelJWT tm = JwtHelper.SerializeJWT(tokenHeader);
+
+            //授权
+            var claimList = new List<Claim>();
+            var claim = new Claim(ClaimTypes.Role, tm.Role);
+            claimList.Add(claim);
+            var identity = new ClaimsIdentity(claimList);
+            var principal = new ClaimsPrincipal(identity);
+            httpContext.User = principal;
+
+            return _next(httpContext);
+        }
+
+    }
+}
+
