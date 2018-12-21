c396e4eb6a089b5e58bdd1a4f24c93dd281540a9
v-anzha <v-anzha@microsoft.com>
2018-12-21 15:46:51 +0800

    note: startup.cs

--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -309,7 +309,15 @@ namespace Blog.Core
         {
             if (env.IsDevelopment())
             {
+                // 在开发环境中，使用异常页面，这样可以暴露错误堆栈信息，所以不要放在生产环境。
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // 在非开发环境中，使用HTTP严格安全传输(or HSTS) 对于保护web安全是非常重要的。
+                // 强制实施 HTTPS 在 ASP.NET Core
+                //app.UseHsts();
 
             }
 
@@ -329,17 +337,28 @@ namespace Blog.Core
             });
             #endregion
 
+            #region Authen
             //app.UseMiddleware<JwtTokenAuth>();//注意此授权方法已经放弃，请使用下边的官方验证方法。但是如果你还想传User的全局变量，还是可以继续使用中间件
             app.UseAuthentication();
+            #endregion
 
+            #region CORS
             //跨域第二种方法，使用策略，详细策略信息在ConfigureService中
             app.UseCors("LimitRequests");//将 CORS 中间件添加到 web 应用程序管线中, 以允许跨域请求。
 
 
             //跨域第一种版本，请要ConfigureService中配置服务 services.AddCors();
             //    app.UseCors(options => options.WithOrigins("http://localhost:8021").AllowAnyHeader()
-            //.AllowAnyMethod());
+            //.AllowAnyMethod()); 
+            #endregion
 
+            // 跳转https
+            app.UseHttpsRedirection();
+            // 使用静态文件
+            app.UseStaticFiles();
+            // 使用cookie
+            app.UseCookiePolicy();
+            // 返回错误码
             app.UseStatusCodePages();//把错误码返回前台，比如是404
 
             app.UseMvc();
