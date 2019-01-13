9c2ccd1a08c6082018708e943d60dfe38f1be9a2
anjoy8 <3143422472@qq.com>
2019-01-13 23:29:11 +0800

    Update Startup.cs

--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -308,7 +308,7 @@ namespace Blog.Core
                       .InstancePerLifetimeScope()
                       .EnableInterfaceInterceptors()//引用Autofac.Extras.DynamicProxy;
                                                     // 如果你想注入两个，就这么写  InterceptedBy(typeof(BlogCacheAOP), typeof(BlogLogAOP));
-                      .InterceptedBy(typeof(BlogCacheAOP),typeof(BlogLogAOP));//允许将拦截器服务的列表分配给注册。
+                      .InterceptedBy(typeof(BlogCacheAOP));//允许将拦截器服务的列表分配给注册。
 
             var repositoryDllFile = Path.Combine(basePath, "Blog.Core.Repository.dll");
             var assemblysRepository = Assembly.LoadFile(repositoryDllFile);
