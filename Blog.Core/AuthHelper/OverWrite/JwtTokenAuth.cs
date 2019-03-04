2dffb7e659916f4e544e06e0e5af3b2d76cf0531
anjoy8 <3143422472@qq.com>
2019-03-04 16:18:33 +0800

    Update JwtTokenAuth.cs

--- Blog.Core/AuthHelper/OverWrite/JwtTokenAuth.cs
+++ Blog.Core/AuthHelper/OverWrite/JwtTokenAuth.cs
@@ -24,6 +24,19 @@ namespace Blog.Core.AuthHelper
         {
             _next = next;
         }
+
+
+        private void PreProceed(HttpContext next)
+        {
+            Console.WriteLine($"{DateTime.Now} middleware invoke preproceed");
+            //...
+        }
+        private void PostProceed(HttpContext next)
+        {
+            Console.WriteLine($"{DateTime.Now} middleware invoke postproceed");
+            //....
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -31,26 +44,43 @@ namespace Blog.Core.AuthHelper
         /// <returns></returns>
         public Task Invoke(HttpContext httpContext)
         {
+            PreProceed(httpContext);
+
+
             //检测是否包含'Authorization'请求头
             if (!httpContext.Request.Headers.ContainsKey("Authorization"))
             {
+                PostProceed(httpContext);
+
                 return _next(httpContext);
             }
             //var tokenHeader = httpContext.Request.Headers["Authorization"].ToString();
             var tokenHeader = httpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
 
-            if (tokenHeader.Length >= 128)
+            try
             {
-                TokenModelJWT tm = JwtHelper.SerializeJWT(tokenHeader);
+                if (tokenHeader.Length >= 128)
+                {
+                    TokenModelJWT tm = JwtHelper.SerializeJWT(tokenHeader);
 
-                //授权
-                var claimList = new List<Claim>();
-                var claim = new Claim(ClaimTypes.Role, tm.Role);
-                claimList.Add(claim);
-                var identity = new ClaimsIdentity(claimList);
-                var principal = new ClaimsPrincipal(identity);
-                httpContext.User = principal;
+                    //授权
+                    var claimList = new List<Claim>();
+                    var claim = new Claim(ClaimTypes.Role, tm.Role);
+                    claimList.Add(claim);
+                    var identity = new ClaimsIdentity(claimList);
+                    var principal = new ClaimsPrincipal(identity);
+                    httpContext.User = principal;
+                }
+
             }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{DateTime.Now} middleware wrong:{e.Message}");
+            }
+
+
+            PostProceed(httpContext);
+
 
             return _next(httpContext);
         }
