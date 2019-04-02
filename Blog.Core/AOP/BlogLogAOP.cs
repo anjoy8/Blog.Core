75ddf6d2bc75ba1ff8c10ce39a0eb3f3cc05ff87
anjoy8 <3143422472@qq.com>
2019-04-02 18:16:11 +0800

    Update BlogLogAOP.cs

--- Blog.Core/AOP/BlogLogAOP.cs
+++ Blog.Core/AOP/BlogLogAOP.cs
@@ -3,6 +3,8 @@ using StackExchange.Profiling;
 using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Blog.Core.AOP
 {
@@ -11,6 +13,11 @@ namespace Blog.Core.AOP
     /// </summary>
     public class BlogLogAOP : IInterceptor
     {
+
+        static ReaderWriterLockSlim LogWriteLock = new ReaderWriterLockSlim();
+        static int LogCount = 100;
+        static int WritedCount = 0;
+        static int FailedCount = 0;
 
         /// <summary>
         /// 实例化IInterceptor唯一方法 
@@ -39,21 +46,69 @@ namespace Blog.Core.AOP
 
             dataIntercept += ($"方法执行完毕，返回结果：{invocation.ReturnValue}");
 
-            #region 输出到当前项目日志
-            var path = Directory.GetCurrentDirectory() + @"\Log";
-            if (!Directory.Exists(path))
+            Parallel.For(0, 1, e =>
             {
-                Directory.CreateDirectory(path);
-            }
+                OutSql2Log(dataIntercept);
+            });
 
-            string fileName = path + $@"\AOPLog.log";
+            #region //输出到当前项目日志,多线程可能会出现争抢资源的问题，舍弃//
+            //var path = Directory.GetCurrentDirectory() + @"\Log";
+            //if (!Directory.Exists(path))
+            //{
+            //    Directory.CreateDirectory(path);
+            //}
 
-            StreamWriter sw = File.AppendText(fileName);
-            sw.WriteLine(dataIntercept);
-            sw.WriteLine();
-            sw.Close();
+            //string fileName = path + $@"\AOPLog.log";
+
+            //StreamWriter sw = File.AppendText(fileName);
+            //sw.WriteLine(dataIntercept);
+            //sw.WriteLine();
+            //sw.Close();
             #endregion
 
         }
+
+        static void OutSql2Log(string dataIntercept)
+        {
+            try
+            {
+                //设置读写锁为写入模式独占资源，其他写入请求需要等待本次写入结束之后才能继续写入
+                //注意：长时间持有读线程锁或写线程锁会使其他线程发生饥饿 (starve)。 为了得到最好的性能，需要考虑重新构造应用程序以将写访问的持续时间减少到最小。
+                //      从性能方面考虑，请求进入写入模式应该紧跟文件操作之前，在此处进入写入模式仅是为了降低代码复杂度
+                //      因进入与退出写入模式应在同一个try finally语句块内，所以在请求进入写入模式之前不能触发异常，否则释放次数大于请求次数将会触发异常
+                LogWriteLock.EnterWriteLock();
+
+                var path = Directory.GetCurrentDirectory() + @"\Log";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                string logFilePath = path + $@"\AOPLog.log";
+
+                var now = DateTime.Now;
+                var logContent = string.Format(
+                    "--------------------------------\n" +
+                    DateTime.Now + "\n" +
+                    dataIntercept + "\n" +
+                    "--------------------------------\n"
+                    );
+
+                File.AppendAllText(logFilePath, logContent);
+                WritedCount++;
+            }
+            catch (Exception)
+            {
+                FailedCount++;
+            }
+            finally
+            {
+                //退出写入模式，释放资源占用
+                //注意：一次请求对应一次释放
+                //      若释放次数大于请求次数将会触发异常[写入锁定未经保持即被释放]
+                //      若请求处理完成后未释放将会触发异常[此模式不下允许以递归方式获取写入锁定]
+                LogWriteLock.ExitWriteLock();
+            }
+        }
+
     }
 }
