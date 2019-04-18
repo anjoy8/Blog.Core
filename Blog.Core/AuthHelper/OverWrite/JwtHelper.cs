8ce5eac9eac8c0201e75f27a96b53eb422c607d1
anjoy8 <3143422472@qq.com>
2019-04-18 17:52:02 +0800

    Update JwtHelper.cs

--- Blog.Core/AuthHelper/OverWrite/JwtHelper.cs
+++ Blog.Core/AuthHelper/OverWrite/JwtHelper.cs
@@ -30,8 +30,8 @@ namespace Blog.Core.AuthHelper.OverWrite
                 new Claim(JwtRegisteredClaimNames.Jti, tokenModel.Uid.ToString()),
                 new Claim(JwtRegisteredClaimNames.Iat, $"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}"),
                 new Claim(JwtRegisteredClaimNames.Nbf,$"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}") ,
-                //这个就是过期时间，目前是过期100秒，可自定义，注意JWT有自己的缓冲过期时间
-                new Claim (JwtRegisteredClaimNames.Exp,$"{new DateTimeOffset(DateTime.Now.AddSeconds(100)).ToUnixTimeSeconds()}"),
+                //这个就是过期时间，目前是过期1000秒，可自定义，注意JWT有自己的缓冲过期时间
+                new Claim (JwtRegisteredClaimNames.Exp,$"{new DateTimeOffset(DateTime.Now.AddSeconds(1000)).ToUnixTimeSeconds()}"),
                 new Claim(JwtRegisteredClaimNames.Iss,iss),
                 new Claim(JwtRegisteredClaimNames.Aud,aud),
                 
