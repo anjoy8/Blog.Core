59de3001d3c3db48507cf4be30d21479d81431d5
anjoy8 <3143422472@qq.com>
2019-04-26 15:54:05 +0800

    update appsettings.json Helper

--- Blog.Core.Common/Helper/Appsettings.cs
+++ Blog.Core.Common/Helper/Appsettings.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Blog.Core.Common
@@ -29,9 +30,16 @@ namespace Blog.Core.Common
                 //Path = $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json";
             }
 
+            //Configuration = new ConfigurationBuilder()
+            //.Add(new JsonConfigurationSource { Path = Path, ReloadOnChange = true })//请注意要把当前appsetting.json 文件->右键->属性->复制到输出目录->始终复制
+            //.Build();
+
             Configuration = new ConfigurationBuilder()
-            .Add(new JsonConfigurationSource { Path = Path, ReloadOnChange = true })//请注意要把当前appsetting.json 文件->右键->属性->复制到输出目录->始终复制
-            .Build();
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .Add(new JsonConfigurationSource { Path = Path, Optional = false, ReloadOnChange = true })//这样的话，可以直接读目录里的json文件，而不是 bin 文件夹下的，所以不用修改复制属性
+               .Build();
+
+
         }
 
         /// <summary>
