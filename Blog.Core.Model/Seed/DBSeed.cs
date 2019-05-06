947b55f79e8abfec5feef2c7779b7fd393ff8b90
anjoy8 <3143422472@qq.com>
2019-05-06 15:51:26 +0800

    Update DBSeed.cs

--- Blog.Core.Model/Seed/DBSeed.cs
+++ Blog.Core.Model/Seed/DBSeed.cs
@@ -43,13 +43,20 @@ namespace Blog.Core.Model.Models
                 //myContext.Db.CodeFirst.InitTables(typeof(Permission)); 
                 //myContext.Db.CodeFirst.InitTables(typeof(Advertisement));
 
+                Console.WriteLine("Database:WMBlog created success!");
+                Console.WriteLine();
 
-
+                Console.WriteLine("Seeding database...");
 
                 #region BlogArticle
                 if (!await myContext.Db.Queryable<BlogArticle>().AnyAsync())
                 {
-                    myContext.GetEntityDB<BlogArticle>().InsertRange(JsonHelper.ParseFormByJson<List<BlogArticle>>(GetNetData.Get(string.Format(GitJsonFileFormat,"BlogArticle"))));
+                    myContext.GetEntityDB<BlogArticle>().InsertRange(JsonHelper.ParseFormByJson<List<BlogArticle>>(GetNetData.Get(string.Format(GitJsonFileFormat, "BlogArticle"))));
+                    Console.WriteLine("Table:BlogArticle created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:BlogArticle already exists...");
                 }
                 #endregion
 
@@ -57,7 +64,12 @@ namespace Blog.Core.Model.Models
                 #region Module
                 if (!await myContext.Db.Queryable<Module>().AnyAsync())
                 {
-                    myContext.GetEntityDB<Module>().InsertRange(JsonHelper.ParseFormByJson<List<Module>>(GetNetData.Get(string.Format(GitJsonFileFormat,"Module"))));
+                    myContext.GetEntityDB<Module>().InsertRange(JsonHelper.ParseFormByJson<List<Module>>(GetNetData.Get(string.Format(GitJsonFileFormat, "Module"))));
+                    Console.WriteLine("Table:Module created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:Module already exists...");
                 }
                 #endregion
 
@@ -65,7 +77,12 @@ namespace Blog.Core.Model.Models
                 #region Permission
                 if (!await myContext.Db.Queryable<Permission>().AnyAsync())
                 {
-                    myContext.GetEntityDB<Permission>().InsertRange(JsonHelper.ParseFormByJson<List<Permission>>(GetNetData.Get(string.Format(GitJsonFileFormat,"Permission"))));
+                    myContext.GetEntityDB<Permission>().InsertRange(JsonHelper.ParseFormByJson<List<Permission>>(GetNetData.Get(string.Format(GitJsonFileFormat, "Permission"))));
+                    Console.WriteLine("Table:Permission created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:Permission already exists...");
                 }
                 #endregion
 
@@ -73,7 +90,12 @@ namespace Blog.Core.Model.Models
                 #region Role
                 if (!await myContext.Db.Queryable<Role>().AnyAsync())
                 {
-                    myContext.GetEntityDB<Role>().InsertRange(JsonHelper.ParseFormByJson<List<Role>>(GetNetData.Get(string.Format(GitJsonFileFormat,"Role"))));
+                    myContext.GetEntityDB<Role>().InsertRange(JsonHelper.ParseFormByJson<List<Role>>(GetNetData.Get(string.Format(GitJsonFileFormat, "Role"))));
+                    Console.WriteLine("Table:Role created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:Role already exists...");
                 }
                 #endregion
 
@@ -81,7 +103,12 @@ namespace Blog.Core.Model.Models
                 #region RoleModulePermission
                 if (!await myContext.Db.Queryable<RoleModulePermission>().AnyAsync())
                 {
-                    myContext.GetEntityDB<RoleModulePermission>().InsertRange(JsonHelper.ParseFormByJson<List<RoleModulePermission>>(GetNetData.Get(string.Format(GitJsonFileFormat,"RoleModulePermission"))));
+                    myContext.GetEntityDB<RoleModulePermission>().InsertRange(JsonHelper.ParseFormByJson<List<RoleModulePermission>>(GetNetData.Get(string.Format(GitJsonFileFormat, "RoleModulePermission"))));
+                    Console.WriteLine("Table:RoleModulePermission created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:RoleModulePermission already exists...");
                 }
                 #endregion
 
@@ -89,7 +116,12 @@ namespace Blog.Core.Model.Models
                 #region Topic
                 if (!await myContext.Db.Queryable<Topic>().AnyAsync())
                 {
-                    myContext.GetEntityDB<Topic>().InsertRange(JsonHelper.ParseFormByJson<List<Topic>>(GetNetData.Get(string.Format(GitJsonFileFormat,"Topic"))));
+                    myContext.GetEntityDB<Topic>().InsertRange(JsonHelper.ParseFormByJson<List<Topic>>(GetNetData.Get(string.Format(GitJsonFileFormat, "Topic"))));
+                    Console.WriteLine("Table:Topic created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:Topic already exists...");
                 }
                 #endregion
 
@@ -97,7 +129,12 @@ namespace Blog.Core.Model.Models
                 #region TopicDetail
                 if (!await myContext.Db.Queryable<TopicDetail>().AnyAsync())
                 {
-                    myContext.GetEntityDB<TopicDetail>().InsertRange(JsonHelper.ParseFormByJson<List<TopicDetail>>(GetNetData.Get(string.Format(GitJsonFileFormat,"TopicDetail"))));
+                    myContext.GetEntityDB<TopicDetail>().InsertRange(JsonHelper.ParseFormByJson<List<TopicDetail>>(GetNetData.Get(string.Format(GitJsonFileFormat, "TopicDetail"))));
+                    Console.WriteLine("Table:TopicDetail created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:TopicDetail already exists...");
                 }
                 #endregion
 
@@ -105,7 +142,12 @@ namespace Blog.Core.Model.Models
                 #region UserRole
                 if (!await myContext.Db.Queryable<UserRole>().AnyAsync())
                 {
-                    myContext.GetEntityDB<UserRole>().InsertRange(JsonHelper.ParseFormByJson<List<UserRole>>(GetNetData.Get(string.Format(GitJsonFileFormat,"UserRole"))));
+                    myContext.GetEntityDB<UserRole>().InsertRange(JsonHelper.ParseFormByJson<List<UserRole>>(GetNetData.Get(string.Format(GitJsonFileFormat, "UserRole"))));
+                    Console.WriteLine("Table:UserRole created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:UserRole already exists...");
                 }
                 #endregion
 
@@ -113,17 +155,23 @@ namespace Blog.Core.Model.Models
                 #region sysUserInfo
                 if (!await myContext.Db.Queryable<sysUserInfo>().AnyAsync())
                 {
-                    myContext.GetEntityDB<sysUserInfo>().InsertRange(JsonHelper.ParseFormByJson<List<sysUserInfo>>(GetNetData.Get(string.Format(GitJsonFileFormat,"sysUserInfo"))));
+                    myContext.GetEntityDB<sysUserInfo>().InsertRange(JsonHelper.ParseFormByJson<List<sysUserInfo>>(GetNetData.Get(string.Format(GitJsonFileFormat, "sysUserInfo"))));
+                    Console.WriteLine("Table:sysUserInfo created success!");
+                }
+                else
+                {
+                    Console.WriteLine("Table:sysUserInfo already exists...");
                 }
                 #endregion
 
 
-
+                Console.WriteLine("Done seeding database.");
+                Console.WriteLine();
 
             }
             catch (Exception ex)
             {
-                throw new Exception("1、注意要先创建空的数据库\n2、"+ex.Message);
+                throw new Exception("1、注意要先创建空的数据库\n2、" + ex.Message);
             }
         }
     }
