371127cbdf476adf2c6570154098bb855e5ca968
anjoy8 <3143422472@qq.com>
2019-05-06 16:38:44 +0800

    Update Startup.cs

--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -103,8 +103,10 @@ namespace Blog.Core
                 //一般采用这种方法
                 c.AddPolicy("LimitRequests", policy =>
                 {
+                    // 支持多个域名端口，注意端口号后不要带/斜杆：比如localhost:8000/，是错的
+                    // 注意，http://127.0.0.1:1818 和 http://localhost:1818 是不一样的，尽量写两个
                     policy
-                    .WithOrigins("http://127.0.0.1:1818", "http://localhost:8080", "http://localhost:8021", "http://localhost:8081", "http://localhost:1818")//支持多个域名端口，注意端口号后不要带/斜杆：比如localhost:8000/，是错的
+                    .WithOrigins("http://127.0.0.1:1818", "http://localhost:8080", "http://localhost:8021", "http://localhost:8081", "http://localhost:1818")
                     .AllowAnyHeader()//Ensures that the policy allows any header.
                     .AllowAnyMethod();
                 });
