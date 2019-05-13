20d11de29b12fd105e33b27d4558272a05a4c192
anjoy8 <3143422472@qq.com>
2019-05-13 18:30:18 +0800

    Update ClaimsController.cs

--- Blog.Core/Controllers/ClaimsController.cs
+++ Blog.Core/Controllers/ClaimsController.cs
@@ -52,7 +52,7 @@ namespace Blog.Core.Controllers
         /// <param name="ids"></param>
         /// <returns></returns>
         [HttpDelete]
-        public IEnumerable<string> BatchDelete(string[] ids)
+        public string BatchDelete(string ids)
         {
             return ids;
         }
