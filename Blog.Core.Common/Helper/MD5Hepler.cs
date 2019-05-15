5251276c584f8e8326b65b06238df2df300ea5e1
anjoy8 <3143422472@qq.com>
2019-05-15 13:04:22 +0800

    Update MD5Hepler.cs

--- Blog.Core.Common/Helper/MD5Hepler.cs
+++ Blog.Core.Common/Helper/MD5Hepler.cs
@@ -28,15 +28,22 @@ namespace Blog.Core.Common.Helper
         public static string MD5Encrypt32(string password = "")
         {
             string pwd = "";
-            string cl = password;
-            MD5 md5 = MD5.Create(); //实例化一个md5对像
-                                    // 加密后是一个字节类型的数组，这里要注意编码UTF8/Unicode等的选择　
-            byte[] s = md5.ComputeHash(Encoding.UTF8.GetBytes(cl));
-            // 通过使用循环，将字节类型的数组转换为字符串，此字符串是常规字符格式化所得
-            for (int i = 0; i < s.Length; i++)
+            try
             {
-                // 将得到的字符串使用十六进制类型格式。格式后的字符是小写的字母，如果使用大写（X）则格式后的字符是大写字符 
-                pwd = pwd + s[i].ToString("X");
+                string cl = password;
+                MD5 md5 = MD5.Create(); //实例化一个md5对像
+                                        // 加密后是一个字节类型的数组，这里要注意编码UTF8/Unicode等的选择　
+                byte[] s = md5.ComputeHash(Encoding.UTF8.GetBytes(cl));
+                // 通过使用循环，将字节类型的数组转换为字符串，此字符串是常规字符格式化所得
+                for (int i = 0; i < s.Length; i++)
+                {
+                    // 将得到的字符串使用十六进制类型格式。格式后的字符是小写的字母，如果使用大写（X）则格式后的字符是大写字符 
+                    pwd = pwd + s[i].ToString("X");
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception($"错误的 password 字符串:{password}");
             }
             return pwd;
         }
