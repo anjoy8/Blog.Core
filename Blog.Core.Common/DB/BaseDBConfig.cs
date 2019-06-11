953a6c9c3ecb8201ce1158d9b92dbe106ed0fd98
anjoy8 <3143422472@qq.com>
2019-06-11 13:12:22 +0800

    Update BaseDBConfig.cs

--- Blog.Core.Common/DB/BaseDBConfig.cs
+++ Blog.Core.Common/DB/BaseDBConfig.cs
@@ -28,26 +28,42 @@ namespace Blog.Core.Common.DB
                 DbType = DataBaseType.Sqlite;
                 return sqliteConnection;
             }
-           else if (isSqlServerEnabled)
+            else if (isSqlServerEnabled)
             {
                 DbType = DataBaseType.SqlServer;
-                return File.Exists(@"D:\my-file\dbCountPsw1.txt") ? File.ReadAllText(@"D:\my-file\dbCountPsw1.txt").Trim() : sqlServerConnection;
+                return DifDBConnOfSecurity(@"D:\my-file\dbCountPsw1.txt", @"c:\my-file\dbCountPsw1.txt", sqlServerConnection);
             }
             else if (isMySqlEnabled)
             {
                 DbType = DataBaseType.MySql;
-                return File.Exists(@"D:\my-file\dbCountPsw1_MySqlConn.txt") ? File.ReadAllText(@"D:\my-file\dbCountPsw1_MySqlConn.txt").Trim() : mySqlConnection;
+                return DifDBConnOfSecurity(@"D:\my-file\dbCountPsw1_MySqlConn.txt", @"c:\my-file\dbCountPsw1_MySqlConn.txt", mySqlConnection);
             }
             else if (IsOracleEnabled)
             {
                 DbType = DataBaseType.Oracle;
-                return File.Exists(@"D:\my-file\dbCountPsw1_OracleConn.txt") ? File.ReadAllText(@"D:\my-file\dbCountPsw1_OracleConn.txt").Trim() : oracleConnection;
+                return DifDBConnOfSecurity(@"D:\my-file\dbCountPsw1_OracleConn.txt", @"c:\my-file\dbCountPsw1_OracleConn.txt", oracleConnection);
             }
             else
             {
                 return "server=.;uid=sa;pwd=sa;database=WMBlogDB";
             }
 
+        }
+        private static string DifDBConnOfSecurity(params string[] conn)
+        {
+            foreach (var item in conn)
+            {
+                try
+                {
+                    if (File.Exists(item))
+                    {
+                        return File.ReadAllText(item).Trim();
+                    }
+                }
+                catch (System.Exception) { }
+            }
+
+            return conn[conn.Length - 1];
         }
 
     }
