55c1866ce01b37fc6732b5bb62a0b4deae19bf57
anjoy <3143422472@qq.com>
2019-07-17 15:36:24 +0800

    Update Startup.cs

--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -419,6 +419,7 @@ namespace Blog.Core
             #region 没有接口层的服务层注入
 
             ////因为没有接口层，所以不能实现解耦，只能用 Load 方法。
+            ////注意如果使用没有接口的服务，并想对其使用 AOP 拦截，就必须设置为虚方法
             ////var assemblysServicesNoInterfaces = Assembly.Load("Blog.Core.Services");
             ////builder.RegisterAssemblyTypes(assemblysServicesNoInterfaces);
 
