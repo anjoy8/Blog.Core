6c0b94d735e14fdf433f116de1ea61080ad6e827
anjoy <3143422472@qq.com>
2019-08-02 16:34:03 +0800

    Update Startup.cs

--- Blog.Core/Startup.cs
+++ Blog.Core/Startup.cs
@@ -131,8 +131,13 @@ namespace Blog.Core
             services.AddMiniProfiler(options =>
                 {
                     options.RouteBasePath = "/profiler";
-                    (options.Storage as MemoryCacheStorage).CacheDuration = TimeSpan.FromMinutes(10);
+                    //(options.Storage as MemoryCacheStorage).CacheDuration = TimeSpan.FromMinutes(10);
+                    options.PopupRenderPosition = StackExchange.Profiling.RenderPosition.Left;
+                    options.PopupShowTimeWithChildren = true;
 
+                    // 可以增加权限
+                    //options.ResultsAuthorize = request => request.HttpContext.User.IsInRole("Admin");
+                    //options.UserIdProvider = request => request.HttpContext.User.Identity.Name;
                 }
             );
 
