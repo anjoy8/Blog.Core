90afbe4c8b8fb91c0447ab54ccf94a1911d3c06e
ansonzhang <3143422472@qq.com>
2019-09-15 09:58:55 +0800

    Update CreateYourProject.bat

--- CreateYourProject.bat
+++ CreateYourProject.bat
@@ -1,6 +1,8 @@
-dotnet new -i Blog.Core.Webapi.Template.1.1.1.nupkg
+color 3
 
-set /p OP=Please set your project name:
+dotnet new -i Blog.Core.Webapi.Template::1.1.1
+
+set /p OP=Please set your project name(for example£ºBaidu.Api):
 
 md .1YourProject
 
@@ -10,6 +12,12 @@ dotnet new blogcoretpl -n %OP%
 
 cd ../
 
+
+echo "Create Successfully!!!! ^ please see the folder .1YourProject"
+
 dotnet new -u Blog.Core.Webapi.Template
+
+
+echo "Delete Template Successfully"
 
 pause
