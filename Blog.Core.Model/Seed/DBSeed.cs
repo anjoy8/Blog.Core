5064f5b4ac68d53b120bed4db2695fbf014e041f
ansonzhang <3143422472@qq.com>
2019-09-27 16:15:15 +0800

    Update DBSeed.cs

--- Blog.Core.Model/Seed/DBSeed.cs
+++ Blog.Core.Model/Seed/DBSeed.cs
@@ -10,7 +10,16 @@ namespace Blog.Core.Model.Models
 {
     public class DBSeed
     {
+             // 这是我的在线demo数据，比较多，且杂乱
+        // 国内网络不好的，可以使用这个 gitee 上的地址：https://gitee.com/laozhangIsPhi/Blog.Data.Share/tree/master/Blog.Core.Data.json/{0}.tsv
         private static string GitJsonFileFormat = "https://github.com/anjoy8/Blog.Data.Share/raw/master/Blog.Core.Data.json/{0}.tsv";
+
+
+        // 这里我把重要的权限数据提出来的精简版，默认一个Admin_Role + 一个管理员用户，
+        // 然后就是菜单+接口+权限分配，注意没有其他博客信息了，下边seeddata 的时候，删掉即可。
+        // 国内网络不好的，可以使用这个 gitee 上的地址：https://gitee.com/laozhangIsPhi/Blog.Data.Share/tree/master/Student.Achieve.json/{0}.tsv
+        private static string GitJsonFileFormat2 = "https://github.com/anjoy8/Blog.Data.Share/raw/master/Student.Achieve.json/{0}.tsv";
+
         /// <summary>
         /// 异步添加种子数据
         /// </summary>
