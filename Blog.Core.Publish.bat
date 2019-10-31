66599c531499c6430519e316a72c385f51a6dcec
anjoy <3143422472@qq.com>
2019-10-31 14:44:42 +0800

    Update Blog.Core.Publish.bat

--- Blog.Core.Publish.bat
+++ Blog.Core.Publish.bat
@@ -8,11 +8,11 @@ dotnet build
 
 cd Blog.Core
 
-dotnet publish -o ..\Blog.Core\bin\Debug\netcoreapp2.2\
+dotnet publish -o ..\Blog.Core\bin\Debug\netcoreapp3.0\
 
 md ..\.PublishFiles
 
-xcopy ..\Blog.Core\bin\Debug\netcoreapp2.2\*.* ..\.PublishFiles\ /s /e 
+xcopy ..\Blog.Core\bin\Debug\netcoreapp3.0\*.* ..\.PublishFiles\ /s /e 
 
 echo "Successfully!!!! ^ please see the file .PublishFiles"
 
