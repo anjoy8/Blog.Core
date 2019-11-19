1df500bfaa932f322fe2db9d3490b66d2d86527e
ansonzhang <3143422472@qq.com>
2019-11-19 21:12:42 +0800

    Update BlogArticleServices.cs

--- Blog.Core.Services/BlogArticleServices.cs
+++ Blog.Core.Services/BlogArticleServices.cs
@@ -30,7 +30,7 @@ namespace Blog.Core.Services
         /// <returns></returns>
         public async Task<BlogViewModels> GetBlogDetails(int id)
         {
-            var bloglist = await base.Query(a => a.bID > 0, a => a.bID);
+            var bloglist = await base.Query(a => a.IsDeleted==false, a => a.bID);
             var blogArticle = (await base.Query(a => a.bID == id)).FirstOrDefault();
 
             BlogViewModels models = null;
@@ -46,8 +46,8 @@ namespace Blog.Core.Services
                 {
                     try
                     {
-                        prevblog = blogIndex > 0 ? (((BlogArticle)(bloglist[blogIndex - 1]))) : null;
-                        nextblog = blogIndex + 1 < bloglist.Count() ? (BlogArticle)(bloglist[blogIndex + 1]) : null;
+                        prevblog = blogIndex > 0 ? bloglist[blogIndex - 1] : null;
+                        nextblog = blogIndex + 1 < bloglist.Count() ? bloglist[blogIndex + 1] : null;
 
 
                         // 注意就是这里,mapper
