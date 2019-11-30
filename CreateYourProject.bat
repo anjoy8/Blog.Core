f2f582bdf60a08c2969c7180d81b26cedc8a4eae
ansonzhang <3143422472@qq.com>
2019-11-30 20:10:14 +0800

    Update Template
    更新项目模板

--- CreateYourProject.bat
+++ CreateYourProject.bat
@@ -1,6 +1,6 @@
 color 3
 
-dotnet new -i .template.config\Blog.Core.Webapi.Template.1.1.4.nupkg
+dotnet new -i .template.config\Blog.Core.Webapi.Template.1.11.30.nupkg
 
 set /p OP=Please set your project name(for example:Baidu.Api):
 
