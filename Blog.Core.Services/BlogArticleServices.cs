3816e6c47bef448e9dff3c8b30135d8b4bcec82d
RLei123 <550919310@qq.com>
2020-04-07 11:56:01 +0800

    优化services层获取博客上一篇下一篇逻辑

--- Blog.Core.Services/BlogArticleServices.cs
+++ Blog.Core.Services/BlogArticleServices.cs
@@ -31,45 +31,62 @@ namespace Blog.Core.Services
         public async Task<BlogViewModels> GetBlogDetails(int id)
         {
             // 此处想获取上一条下一条数据，因此将全部数据list出来，有好的想法请提出
-            var bloglist = await base.Query(a => a.IsDeleted==false, a => a.bID);
+            //var bloglist = await base.Query(a => a.IsDeleted==false, a => a.bID);
             var blogArticle = (await base.Query(a => a.bID == id)).FirstOrDefault();
 
             BlogViewModels models = null;
 
             if (blogArticle != null)
             {
-                BlogArticle prevblog;
-                BlogArticle nextblog;
+                models = _mapper.Map<BlogViewModels>(blogArticle);
 
-
-                int blogIndex = bloglist.FindIndex(item => item.bID == id);
-                if (blogIndex >= 0)
+                //要取下一篇和上一篇，以当前id开始，按id排序后top(2)，而不用取出所有记录
+                //这样在记录很多的时候也不会有多大影响
+                var nextBlogs = await base.Query($"bId>={id}", 2, "bID");
+                if (nextBlogs.Count == 2)
                 {
-                    try
-                    {
-                        prevblog = blogIndex > 0 ? bloglist[blogIndex - 1] : null;
-                        nextblog = blogIndex + 1 < bloglist.Count() ? bloglist[blogIndex + 1] : null;
-
-
-                        // 注意就是这里,mapper
-                        models = _mapper.Map<BlogViewModels>(blogArticle);
-
-                        if (nextblog != null)
-                        {
-                            models.next = nextblog.btitle;
-                            models.nextID = nextblog.bID;
-                        }
-
-                        if (prevblog != null)
-                        {
-                            models.previous = prevblog.btitle;
-                            models.previousID = prevblog.bID;
-                        }
-                        var entity2Viewmodel = _mapper.Map<BlogArticle>(models);
-
-                    }
-                    catch (Exception ex) { throw new Exception(ex.Message); }
+                    models.next = nextBlogs[1].btitle;
+                    models.nextID = nextBlogs[1].bID;
                 }
+                var prevBlogs = await base.Query($"bId<={id}", 2, "bID desc");
+                if (prevBlogs.Count == 2)
+                {
+                    models.previous = prevBlogs[1].btitle;
+                    models.previousID = prevBlogs[1].bID;
+                }
+
+                //BlogArticle prevblog;
+                //BlogArticle nextblog;
+
+
+                //int blogIndex = bloglist.FindIndex(item => item.bID == id);
+                //if (blogIndex >= 0)
+                //{
+                //    try
+                //    {
+                //        prevblog = blogIndex > 0 ? bloglist[blogIndex - 1] : null;
+                //        nextblog = blogIndex + 1 < bloglist.Count() ? bloglist[blogIndex + 1] : null;
+
+
+                //        // 注意就是这里,mapper
+                //        models = _mapper.Map<BlogViewModels>(blogArticle);
+
+                //        if (nextblog != null)
+                //        {
+                //            models.next = nextblog.btitle;
+                //            models.nextID = nextblog.bID;
+                //        }
+
+                //        if (prevblog != null)
+                //        {
+                //            models.previous = prevblog.btitle;
+                //            models.previousID = prevblog.bID;
+                //        }
+                //        var entity2Viewmodel = _mapper.Map<BlogArticle>(models);
+
+                //    }
+                //    catch (Exception ex) { throw new Exception(ex.Message); }
+                //}
 
 
                 blogArticle.btraffic += 1;
