631298c899386890aea27f573342f406661e6b09
anjoy8 <3143422472@qq.com>
2020-06-01 19:49:17 +0800

    Update IpPolicyRateLimitSetup.cs

--- Blog.Core/Extensions/IpPolicyRateLimitSetup.cs
+++ Blog.Core/Extensions/IpPolicyRateLimitSetup.cs
@@ -14,13 +14,22 @@ namespace Blog.Core.Extensions
         public static void AddIpPolicyRateLimitSetup(this IServiceCollection services, IConfiguration Configuration)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
-
+            
+            // needed to store rate limit counters and ip rules
             services.AddMemoryCache();
-
+            //load general configuration from appsettings.json
             services.Configure<IpRateLimitOptions>(Configuration.GetSection("IpRateLimiting"));
+            // inject counter and rules stores
             services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
             services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+
+            // inject counter and rules distributed cache stores
+            //services.AddSingleton<IIpPolicyStore, DistributedCacheIpPolicyStore>();
+            //services.AddSingleton<IRateLimitCounterStore, DistributedCacheRateLimitCounterStore>();
+
+            // the clientId/clientIp resolvers use it.
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            // configuration (resolvers, counter key builders)
             services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
         }
     }
