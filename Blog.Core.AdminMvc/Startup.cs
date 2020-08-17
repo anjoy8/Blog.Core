0b005fefb5e28b31b146ea856d2bde87b9b234b3
anjoy8 <3143422472@qq.com>
2020-08-17 19:55:22 +0800

    test ocelot

--- Blog.Core.AdminMvc/Startup.cs
+++ Blog.Core.AdminMvc/Startup.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace Blog.Core.AdminMvc
 {
@@ -14,10 +15,13 @@ namespace Blog.Core.AdminMvc
         *│　作    者：anson zhang                                             
         *└──────────────────────────────────────────────────────────────┘
         */
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,15 +32,7 @@ namespace Blog.Core.AdminMvc
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseOcelot().Wait();
         }
     }
 }
