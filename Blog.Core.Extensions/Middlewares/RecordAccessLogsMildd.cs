1c267eca2f237ae8e772646787e43971d2ed09b2
anjoy8 <3143422472@qq.com>
2020-09-06 19:58:50 +0800

    Update RecordAccessLogsMildd.cs

--- Blog.Core.Extensions/Middlewares/RecordAccessLogsMildd.cs
+++ Blog.Core.Extensions/Middlewares/RecordAccessLogsMildd.cs
@@ -25,6 +25,7 @@ namespace Blog.Core.Middlewares
         private readonly RequestDelegate _next;
         private readonly IUser _user;
         private readonly ILogger<RecordAccessLogsMildd> _logger;
+        private Stopwatch _stopwatch;
 
         /// <summary>
         /// 
@@ -35,6 +36,7 @@ namespace Blog.Core.Middlewares
             _next = next;
             _user = user;
             _logger = logger;
+            _stopwatch = new Stopwatch();
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -44,46 +46,67 @@ namespace Blog.Core.Middlewares
                 // 过滤，只有接口
                 if (context.Request.Path.Value.Contains("api"))
                 {
+                    _stopwatch.Restart();
+                    var userAccessModel = new UserAccessModel();
 
-                    //记录Job时间
-                    Stopwatch stopwatch = new Stopwatch();
-                    stopwatch.Start();
+                    HttpRequest request = context.Request;
 
-                    context.Request.EnableBuffering();
-                    Stream originalBody = context.Response.Body;
+                    userAccessModel.API = request.Path.ObjToString().TrimEnd('/').ToLower();
+                    userAccessModel.User = _user.Name;
+                    userAccessModel.IP = IPLogMildd.GetClientIP(context);
+                    userAccessModel.BeginTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                    userAccessModel.RequestMethod = request.Method;
+                    userAccessModel.Agent = request.Headers["User-Agent"].ObjToString();
 
-                    try
+
+                    // 获取请求body内容
+                    if (request.Method.ToLower().Equals("post"))
                     {
+                        // 启用倒带功能，就可以让 Request.Body 可以再次读取
+                        request.EnableBuffering();
 
-                        using (var ms = new MemoryStream())
+                        Stream stream = request.Body;
+                        byte[] buffer = new byte[request.ContentLength.Value];
+                        stream.Read(buffer, 0, buffer.Length);
+                        userAccessModel.RequestData = Encoding.UTF8.GetString(buffer);
+
+                        request.Body.Position = 0;
+                    }
+                    else if (request.Method.ToLower().Equals("get") || request.Method.ToLower().Equals("delete"))
+                    {
+                        userAccessModel.RequestData = HttpUtility.UrlDecode(request.QueryString.ObjToString(), Encoding.UTF8);
+                    }
+
+                    // 获取Response.Body内容
+                    var originalBodyStream = context.Response.Body;
+                    using (var responseBody = new MemoryStream())
+                    {
+                        context.Response.Body = responseBody;
+
+                        await _next(context);
+
+                        var responseBodyData = await GetResponse(context.Response);
+
+                        await responseBody.CopyToAsync(originalBodyStream);
+                    }
+
+                    // 响应完成记录时间和存入日志
+                    context.Response.OnCompleted(() =>
+                    {
+                        _stopwatch.Stop();
+
+                        userAccessModel.OPTime = _stopwatch.ElapsedMilliseconds + "ms";
+
+                        // 自定义log输出
+                        var requestInfo = JsonConvert.SerializeObject(userAccessModel);
+                        Parallel.For(0, 1, e =>
                         {
-                            context.Response.Body = ms;
-
-
-                            await _next(context);
-
-                            ms.Position = 0;
-                            await ms.CopyToAsync(originalBody);
-                        }
-
-                        // 响应完成记录时间和存入日志
-                        context.Response.OnCompleted(async () =>
-                        {
-                            stopwatch.Stop();
-                            var opTime = stopwatch.Elapsed.TotalMilliseconds.ToString("00") + "ms";
-                            // 存储请求数据
-                            await RequestDataLog(context, opTime);
+                            LogLock.OutSql2Log("RecordAccessLogs", new string[] { requestInfo + "," }, false);
                         });
 
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError("请求路径：" + context.Request.Path + ex.Message + "\r\n" + ex.InnerException);
-                    }
-                    finally
-                    {
-                        context.Response.Body = originalBody;
-                    }
+                        return Task.CompletedTask;
+                    });
+
                 }
                 else
                 {
@@ -96,39 +119,18 @@ namespace Blog.Core.Middlewares
             }
         }
 
-        private async Task RequestDataLog(HttpContext context, string opTime)
+
+        /// <summary>
+        /// 获取响应内容
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public async Task<string> GetResponse(HttpResponse response)
         {
-            var request = context.Request;
-            var sr = new StreamReader(request.Body);
-
-            var requestData = request.Method == "GET" || request.Method == "DELETE" ? HttpUtility.UrlDecode(request.QueryString.ObjToString(), Encoding.UTF8) : (await sr.ReadToEndAsync()).ObjToString();
-            if (requestData.IsNotEmptyOrNull() && requestData.Length > 30)
-            {
-                requestData = requestData.Substring(0, 30);
-            }
-
-            var requestInfo = JsonConvert.SerializeObject(new UserAccessModel()
-            {
-                User = _user.Name,
-                IP = IPLogMildd.GetClientIP(context),
-                API = request.Path.ObjToString().TrimEnd('/').ToLower(),
-                BeginTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
-                OPTime = opTime,
-                RequestMethod = request.Method,
-                RequestData = requestData,
-                Agent = request.Headers["User-Agent"].ObjToString()
-            });
-
-            if (!string.IsNullOrEmpty(requestInfo))
-            {
-                // 自定义log输出
-                Parallel.For(0, 1, e =>
-                {
-                    LogLock.OutSql2Log("RecordAccessLogs", new string[] { requestInfo + "," }, false);
-                });
-
-                request.Body.Position = 0;
-            }
+            response.Body.Seek(0, SeekOrigin.Begin);
+            var text = await new StreamReader(response.Body).ReadToEndAsync();
+            response.Body.Seek(0, SeekOrigin.Begin);
+            return text;
         }
     }
 
