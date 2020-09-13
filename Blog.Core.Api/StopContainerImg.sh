e538b7a87d2883779996ae4daefff92482c10255
anjoy8 <3143422472@qq.com>
2020-09-13 19:37:21 +0800

    add stop docker container sh

---
+++ Blog.Core.Api/StopContainerImg.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+docker ps|grep ${1}|while read i;do i; 
+echo "容器已启动,详细信息:${i}";
+docker stop ${1};
+docker rm ${1};
+docker rmi ${2};
+echo "已关闭容器,${1}" ;
+done;
--- Blog.Core.Publish.Docker.Jenkins.sh
+++ Blog.Core.Publish.Docker.Jenkins.sh
@@ -9,8 +9,12 @@ cd bin/Debug/netcoreapp3.1/publish/
 #rm -f appsettings.json
 #\cp -rf /var/jenkins_home/workspace/SecurityConfig/Blog.Core/appsettings.json appsettings.json
 
-docker stop apkcontainer
-docker rm apkcontainer
-docker rmi laozhangisphi/apkimg
+#docker stop apkcontainer
+#docker rm apkcontainer
+#docker rmi laozhangisphi/apkimg
+
+chmod 777 StopContainerImg.sh
+./StopContainerImg.sh apkcontainer laozhangisphi/apkimg
+
 docker build -t laozhangisphi/apkimg .
 docker run --name=apkcontainer -d -v /data/blogcore/appsettings.json:/app/appsettings.json -v /data/blogcore/Log/:/app/Log -v /etc/localtime:/etc/localtime -it -p 8081:8081 laozhangisphi/apkimg
