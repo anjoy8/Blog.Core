0ffd347a3d78d9c63bba9a08aea99e93513ef3f5
anjoy8 <3143422472@qq.com>
2020-10-19 19:34:31 +0800

    Fixed #121 #120  Bug
    修改 #121  #120  bug

--- Blog.Core.Extensions/ServiceExtensions/MiniProfilerSetup.cs
+++ Blog.Core.Extensions/ServiceExtensions/MiniProfilerSetup.cs
@@ -12,19 +12,21 @@ namespace Blog.Core.Extensions
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
 
-            // 3.x使用MiniProfiler，必须要注册MemoryCache服务
-            services.AddMiniProfiler(options =>
-            {
-                options.RouteBasePath = "/profiler";
-                //(options.Storage as MemoryCacheStorage).CacheDuration = TimeSpan.FromMinutes(10);
-                options.PopupRenderPosition = StackExchange.Profiling.RenderPosition.Left;
-                options.PopupShowTimeWithChildren = true;
+            services.AddMiniProfiler();
 
-                // 可以增加权限
-                //options.ResultsAuthorize = request => request.HttpContext.User.IsInRole("Admin");
-                //options.UserIdProvider = request => request.HttpContext.User.Identity.Name;
-            }
-           );
+            // 3.x使用MiniProfiler，必须要注册MemoryCache服务
+            // services.AddMiniProfiler(options =>
+            // {
+            //     options.RouteBasePath = "/profiler";
+            //     //(options.Storage as MemoryCacheStorage).CacheDuration = TimeSpan.FromMinutes(10);
+            //     options.PopupRenderPosition = StackExchange.Profiling.RenderPosition.Left;
+            //     options.PopupShowTimeWithChildren = true;
+
+            //     // 可以增加权限
+            //     //options.ResultsAuthorize = request => request.HttpContext.User.IsInRole("Admin");
+            //     //options.UserIdProvider = request => request.HttpContext.User.Identity.Name;
+            // }
+            //);
         }
     }
 }
