e31fe21b87ec19d0be075f31668d534c5c1b41d0
anjoy8 <3143422472@qq.com>
2020-12-03 03:01:58 +0000

    :m:  Inherit MongoDB functionality
    集成MongoDb功能

---
+++ Blog.Core.Repository/MongoRepository/MongoBaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Repository.MongoRepository
+{
+
+
+    public class MongoBaseRepository<TEntity> : IMongoBaseRepository<TEntity> where TEntity : class, new()
+    {
+        private readonly LocationsContext _context;
+
+        public MongoBaseRepository()
+        {
+            _context = new LocationsContext();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Db.GetCollection<TEntity>(nameof(TEntity))
+               .InsertOneAsync(entity);
+        }
+
+        public async Task<TEntity> GetAsync(int Id)
+        {
+            var filter = Builders<TEntity>.Filter.Eq("Id", Id);
+
+            return await _context.Db.GetCollection<TEntity>(nameof(TEntity))
+                .Find(filter)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<TEntity>> GetListAsync()
+        {
+            return await _context.Db.GetCollection<TEntity>(nameof(TEntity))
+                .Find(new BsonDocument())
+                .ToListAsync();
+        }
+    }
+}
