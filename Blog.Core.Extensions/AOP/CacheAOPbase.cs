06578bfc01426b09e036943d025bdae58cc2ad91
anjoy8 <3143422472@qq.com>
2020-12-04 23:23:43 +0800

    :+1:  sqlsugar expression Resolver

--- Blog.Core.Extensions/AOP/CacheAOPbase.cs
+++ Blog.Core.Extensions/AOP/CacheAOPbase.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.DynamicProxy;
+using SqlSugar;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -65,62 +66,17 @@ namespace Blog.Core.AOP
 
         private static string Resolve(Expression expression)
         {
-            if (expression is LambdaExpression)
+            ExpressionContext expContext = new ExpressionContext();
+            expContext.Resolve(expression, ResolveExpressType.WhereSingle);
+            var value = expContext.Result.GetString();
+            var pars = expContext.Parameters;
+
+            pars.ForEach(s =>
             {
-                LambdaExpression lambda = expression as LambdaExpression;
-                expression = lambda.Body;
-                return Resolve(expression);
-            }
-            if (expression is BinaryExpression)
-            {
-                BinaryExpression binary = expression as BinaryExpression;
-                if (binary.Left is MemberExpression && binary.Right is ConstantExpression)//解析x=>x.Name=="123" x.Age==123这类
-                    return ResolveFunc(binary.Left, binary.Right, binary.NodeType);
-                if (binary.Left is MethodCallExpression && binary.Right is ConstantExpression)//解析x=>x.Name.Contains("xxx")==false这类的
-                {
-                    object value = (binary.Right as ConstantExpression).Value;
-                    return ResolveLinqToObject(binary.Left, value, binary.NodeType);
-                }
-                if ((binary.Left is MemberExpression && binary.Right is MemberExpression)
-                    || (binary.Left is MemberExpression && binary.Right is UnaryExpression))//解析x=>x.Date==DateTime.Now这种
-                {
-                    LambdaExpression lambda = Expression.Lambda(binary.Right);
-                    Delegate fn = lambda.Compile();
-                    ConstantExpression value = Expression.Constant(fn.DynamicInvoke(null), binary.Right.Type);
-                    return ResolveFunc(binary.Left, value, binary.NodeType);
-                }
-            }
-            if (expression is UnaryExpression)
-            {
-                UnaryExpression unary = expression as UnaryExpression;
-                if (unary.Operand is MethodCallExpression)//解析!x=>x.Name.Contains("xxx")或!array.Contains(x.Name)这类
-                    return ResolveLinqToObject(unary.Operand, false);
-                if (unary.Operand is MemberExpression && unary.NodeType == ExpressionType.Not)//解析x=>!x.isDeletion这样的 
-                {
-                    ConstantExpression constant = Expression.Constant(false);
-                    return ResolveFunc(unary.Operand, constant, ExpressionType.Equal);
-                }
-            }
-            if (expression is MemberExpression && expression.NodeType == ExpressionType.MemberAccess)//解析x=>x.isDeletion这样的 
-            {
-                MemberExpression member = expression as MemberExpression;
-                ConstantExpression constant = Expression.Constant(true);
-                return ResolveFunc(member, constant, ExpressionType.Equal);
-            }
-            if (expression is MethodCallExpression)//x=>x.Name.Contains("xxx")或array.Contains(x.Name)这类
-            {
-                MethodCallExpression methodcall = expression as MethodCallExpression;
-                return ResolveLinqToObject(methodcall, true);
-            }
-            var body = expression as BinaryExpression;
-            //已经修改过代码body应该不会是null值了
-            if (body == null)
-                return string.Empty;
-            var Operator = GetOperator(body.NodeType);
-            var Left = Resolve(body.Left);
-            var Right = Resolve(body.Right);
-            string Result = string.Format("({0} {1} {2})", Left, Operator, Right);
-            return Result;
+                value = value.Replace(s.ParameterName, s.Value.ObjToString());
+            });
+
+            return value;
         }
 
         private static string GetOperator(ExpressionType expressiontype)
