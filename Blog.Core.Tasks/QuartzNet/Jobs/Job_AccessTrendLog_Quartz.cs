ad3e1704aab74cb66fb71104de3bcc97dc84fb0e
anjoy8 <3143422472@qq.com>
2021-09-16 11:41:59 +0800

    Update Job_AccessTrendLog_Quartz.cs

--- Blog.Core.Tasks/QuartzNet/Jobs/Job_AccessTrendLog_Quartz.cs
+++ Blog.Core.Tasks/QuartzNet/Jobs/Job_AccessTrendLog_Quartz.cs
@@ -73,7 +73,7 @@ namespace Blog.Core.Tasks
         {
             List<UserAccessFromFIles> userAccessModels = new();
             var accessLogs = LogLock.ReadLog(
-                Path.Combine(_environment.ContentRootPath, "Log"), "RecordAccessLogs_", Encoding.UTF8, ReadType.Prefix
+                Path.Combine(_environment.ContentRootPath, "Log"), "RecordAccessLogs_", Encoding.UTF8, ReadType.Prefix,2
                 ).ObjToString().TrimEnd(',');
 
             try
