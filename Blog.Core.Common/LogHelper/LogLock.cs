e4d948d54e10b6e1986831adcf79d8e86d7d8b35
Nine.Designmini <cuz_mr.j@live.com>
2021-12-01 17:42:10 +0800

    增加log4net写入MongoDB和MSSQL

--- Blog.Core.Common/LogHelper/LogLock.cs
+++ Blog.Core.Common/LogHelper/LogLock.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Core.Common.Helper;
+using log4net;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,7 @@ namespace Blog.Core.Common.LogHelper
 {
     public class LogLock
     {
-
+        private static readonly ILog log = LogManager.GetLogger(typeof(LogLock));
         static ReaderWriterLockSlim LogWriteLock = new ReaderWriterLockSlim();
         static int WritedCount = 0;
         static int FailedCount = 0;
@@ -23,6 +24,19 @@ namespace Blog.Core.Common.LogHelper
         }
 
         public static void OutSql2Log(string prefix, string[] dataParas, bool IsHeader = true, bool isWrt = false)
+        {
+
+            if (Appsettings.app(new string[] { "AppSettings", "LogToDb", "Enabled" }).ObjToBool())
+            {
+                OutSql2LogToDB(prefix, dataParas, IsHeader);
+            }
+            else
+            {
+                OutSql2LogToFile(prefix, dataParas, IsHeader, isWrt);
+            }
+        }
+
+        public static void OutSql2LogToFile(string prefix, string[] dataParas, bool IsHeader = true, bool isWrt = false)
         {
             try
             {
@@ -80,7 +94,40 @@ namespace Blog.Core.Common.LogHelper
                 LogWriteLock.ExitWriteLock();
             }
         }
+        public static void OutSql2LogToDB(string prefix, string[] dataParas, bool IsHeader = true)
+        {
 
+            string logContent = String.Join("\r\n", dataParas);
+            if (IsHeader)
+            {
+                logContent = (
+                   "--------------------------------\r\n" +
+                   DateTime.Now + "|\r\n" +
+                   String.Join("\r\n", dataParas) + "\r\n"
+                   );
+            }
+            switch (prefix)
+            {
+                case "AOPLog":
+                    log.Info(logContent);
+                    break;
+                case "AOPLogEx":
+                    log.Error(logContent);
+                    break;
+                case "RequestIpInfoLog":
+                    log.Debug(logContent);
+                    break;
+                case "RecordAccessLogs":
+                    log.Debug(logContent);
+                    break;
+                case "SqlLog":
+                    log.Info(logContent);
+                    break;
+                default:
+                    break;
+            }
+
+        }
         /// <summary>
         /// 读取文件内容
         /// </summary>
