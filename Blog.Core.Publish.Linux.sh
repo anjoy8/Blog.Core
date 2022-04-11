f03390eac862f8f66b8896b5242c6589b744bcb6
anjoy8 <3143422472@qq.com>
2022-04-11 11:53:44 +0800

    Fixed #263  bug

--- Blog.Core.Publish.Linux.sh
+++ Blog.Core.Publish.Linux.sh
@@ -1,5 +1,5 @@
 git pull;
-rm -rf .PublishFiles;
+find .PublishFiles/ -type f -and ! -path '*/wwwroot/images/*' ! -name 'appsettings.*' |xargs rm -rf
 dotnet build;
 dotnet publish -o /home/Blog.Core/Blog.Core.Api/bin/Debug/net6.0;
 cp -r /home/Blog.Core/Blog.Core.Api/bin/Debug/net6.0 .PublishFiles;
