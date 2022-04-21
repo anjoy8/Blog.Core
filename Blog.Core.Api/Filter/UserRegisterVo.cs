4e07c20e40b3a10b9dc6c28db6445aaff8405e54
anjoy8 <3143422472@qq.com>
2022-04-21 13:19:30 +0800

    Update UserRegisterVo.cs

--- Blog.Core.Api/Filter/UserRegisterVo.cs
+++ Blog.Core.Api/Filter/UserRegisterVo.cs
@@ -12,7 +12,14 @@ namespace Blog.Core.Filter
         public string NickName { get; set; }
 
         public string SourceType { get; set; }
+        public IEnumerable<CarInfo> Cars { get; set; }
 
+    }
+
+    public class CarInfo
+    {
+        public int CarCount { get; set; }
+        public int CarSize { get; set; }
     }
 
     public class UserRegisterVoValidator : AbstractValidator<UserRegisterVo>
@@ -21,8 +28,13 @@ namespace Blog.Core.Filter
         {
             When(x => !string.IsNullOrEmpty(x.NickName) || !string.IsNullOrEmpty(x.Telphone), () =>
             {
-                RuleFor(x => x.NickName).Must(e => IsLegalName(e)).WithMessage("请填写合法的姓名，必须是汉字和字母");
-                RuleFor(x => x.Telphone).Must(e => IsLegalPhone(e)).WithMessage("请填写正确的手机号码");
+                RuleFor(x => x.NickName)
+                    .Must(e => IsLegalName(e)).WithMessage("请填写合法的姓名，必须是汉字和字母");
+                RuleFor(x => x.Telphone)
+                    .Must(e => IsLegalPhone(e)).WithMessage("请填写正确的手机号码");
+                RuleFor(x => x.Cars)
+                    .NotNull().NotEmpty().WithMessage("车辆信息不正确");
+                RuleForEach(x => x.Cars).SetValidator(new CarInfoValidator());
             });
 
         }
@@ -38,6 +50,17 @@ namespace Blog.Core.Filter
             //判断手机号
             const string pattern = "(^1\\d{10}$)";
             return (!string.IsNullOrEmpty(phone) && Regex.IsMatch(phone, pattern));
+        }
+    }
+    public class CarInfoValidator : AbstractValidator<CarInfo>
+    {
+        public CarInfoValidator()
+        {
+            RuleFor(x => x.CarCount)
+                .GreaterThanOrEqualTo(0).WithMessage("车辆数量必须大于等于0")
+                .LessThanOrEqualTo(500).WithMessage($"存在车型数量已达上限");
+            RuleFor(x => x.CarSize)
+                .IsInEnum().WithMessage("车型不正确");
         }
     }
 
