57df64ffb40cce11ec054254af630cc103d3c7b8
LemonNoCry <773596523@qq.com>
2022-07-22 19:31:32 +0800

    ✨⚡️ 动态Lambda 在兼容原有的操作上优化动态查询条件、处理特殊符号
    1.使用正则匹配
    1.连接符支持 &&、&
    2.比较符支持 >、>=、==、=、<、<=、like、contains、in
    3.优化处理特殊符号，一般情况都可以应对
    4.最好使用“”包括值，数值类型可不用““

--- Blog.Core.Common/Helper/UtilConvert.cs
+++ Blog.Core.Common/Helper/UtilConvert.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace Blog.Core
 {
     /// <summary>
@@ -19,8 +22,10 @@ namespace Blog.Core
             {
                 return reval;
             }
+
             return reval;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -34,8 +39,10 @@ namespace Blog.Core
             {
                 return reval;
             }
+
             return errorValue;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -48,8 +55,10 @@ namespace Blog.Core
             {
                 return reval;
             }
+
             return 0;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -63,8 +72,10 @@ namespace Blog.Core
             {
                 return reval;
             }
+
             return errorValue;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -75,6 +86,7 @@ namespace Blog.Core
             if (thisValue != null) return thisValue.ToString().Trim();
             return "";
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -84,6 +96,7 @@ namespace Blog.Core
         {
             return ObjToString(thisValue) != "" && ObjToString(thisValue) != "undefined" && ObjToString(thisValue) != "null";
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -95,6 +108,9 @@ namespace Blog.Core
             if (thisValue != null) return thisValue.ToString().Trim();
             return errorValue;
         }
+
+        public static bool IsNullOrEmpty(this object thisValue) => thisValue == null || thisValue == DBNull.Value || string.IsNullOrWhiteSpace(thisValue.ToString());
+
         /// <summary>
         /// 
         /// </summary>
@@ -107,8 +123,10 @@ namespace Blog.Core
             {
                 return reval;
             }
+
             return 0;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -122,8 +140,10 @@ namespace Blog.Core
             {
                 return reval;
             }
+
             return errorValue;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -136,8 +156,10 @@ namespace Blog.Core
             {
                 reval = Convert.ToDateTime(thisValue);
             }
+
             return reval;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -151,8 +173,10 @@ namespace Blog.Core
             {
                 return reval;
             }
+
             return errorValue;
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -165,6 +189,7 @@ namespace Blog.Core
             {
                 return reval;
             }
+
             return reval;
         }
 
@@ -178,6 +203,78 @@ namespace Blog.Core
         {
             TimeSpan ts = thisValue - new DateTime(1970, 1, 1, 0, 0, 0, 0);
             return Convert.ToInt64(ts.TotalSeconds).ToString();
+        }
+
+        public static object ChangeType(this object value, Type type)
+        {
+            if (value == null && type.IsGenericType) return Activator.CreateInstance(type);
+            if (value == null) return null;
+            if (type == value.GetType()) return value;
+            if (type.IsEnum)
+            {
+                if (value is string)
+                    return Enum.Parse(type, value as string);
+                else
+                    return Enum.ToObject(type, value);
+            }
+
+            if (!type.IsInterface && type.IsGenericType)
+            {
+                Type innerType = type.GetGenericArguments()[0];
+                object innerValue = ChangeType(value, innerType);
+                return Activator.CreateInstance(type, new object[] { innerValue });
+            }
+
+            if (value is string && type == typeof(Guid)) return new Guid(value as string);
+            if (value is string && type == typeof(Version)) return new Version(value as string);
+            if (!(value is IConvertible)) return value;
+            return Convert.ChangeType(value, type);
+        }
+
+        public static object ChangeTypeList(this object value, Type type)
+        {
+            if (value == null) return default;
+
+            var gt = typeof(List<>).MakeGenericType(type);
+            dynamic lis = Activator.CreateInstance(gt);
+
+            var addMethod = gt.GetMethod("Add");
+            string values = value.ToString();
+            if (values != null && values.StartsWith("(") && values.EndsWith(")"))
+            {
+                string[] splits;
+                if (values.Contains("\",\""))
+                {
+                    splits = values.Remove(values.Length - 2, 2)
+                        .Remove(0, 2)
+                        .Split("\",\"");
+                }
+                else
+                {
+                    splits = values.Remove(0, 1)
+                        .Remove(values.Length - 2, 1)
+                        .Split(",");
+                }
+
+                foreach (var split in splits)
+                {
+                    var str = split;
+                    if (split.StartsWith("\"") && split.EndsWith("\""))
+                    {
+                        str = split.Remove(0, 1)
+                            .Remove(split.Length - 2, 1);
+                    }
+
+                    addMethod.Invoke(lis, new object[] { ChangeType(str, type) });
+                }
+            }
+
+            return lis;
+        }
+
+        public static string ToJson(this object value)
+        {
+            return JsonConvert.SerializeObject(value);
         }
     }
 }
