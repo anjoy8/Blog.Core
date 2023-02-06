60653a383ddd3a9f1ba80acb0d6b550742ff6366
Lemon.NoCry <773596523@qq.com>
2023-02-07 00:00:05 +0800

    ✨　增加种子数据接口、增加常用方法

---
+++ Blog.Core.Common/Core/InternalApp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Blog.Core.Common.Core;
+
+public static class InternalApp
+{
+    /// <summary>根服务</summary>
+    public static IServiceProvider RootServices;
+
+    public static void ConfigureApplication(this IHost app)
+    {
+        RootServices = app.Services;
+    }
+}
---
+++ Blog.Core.Common/Extensions/AssemblysExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Blog.Core.Common.Extensions;
+
+public static class AssemblysExtensions
+{
+    public static List<Assembly> GetAllAssemblies()
+    {
+        var list = new List<Assembly>();
+        var deps = DependencyContext.Default;
+        var libs = deps.CompileLibraries.Where(lib => !lib.Serviceable && lib.Type != "package" && lib.Name.StartsWith("Baiqian"));
+        foreach (var lib in libs)
+        {
+            var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(lib.Name));
+            list.Add(assembly);
+        }
+
+        return list;
+    }
+}
