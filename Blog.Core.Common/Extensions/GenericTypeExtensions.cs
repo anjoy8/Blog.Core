1cc3ef25fa1afcb74e159ec18dd65da7ce2a7770
anjoy8 <3143422472@qq.com>
2023-02-14 22:14:41 +0800

    Update DynamicLinqFactory.cs

--- Blog.Core.Common/Extensions/GenericTypeExtensions.cs
+++ Blog.Core.Common/Extensions/GenericTypeExtensions.cs
@@ -33,24 +33,24 @@ namespace Blog.Core.Common.Extensions
         /// <param name="type">类型</param>
         /// <param name="generic">泛型类型</param>
         /// <returns>bool</returns>
-        public static bool HasImplementedRawGeneric(this Type type, Type generic)
-        {
-            // 检查接口类型
-            var isTheRawGenericType = type.GetInterfaces().Any(IsTheRawGenericType);
-            if (isTheRawGenericType) return true;
+        // public static bool HasImplementedRawGeneric(this Type type, Type generic)
+        // {
+        //     // 检查接口类型
+        //     var isTheRawGenericType = type.GetInterfaces().Any(IsTheRawGenericType);
+        //     if (isTheRawGenericType) return true;
 
-            // 检查类型
-            while (type != null && type != typeof(object))
-            {
-                isTheRawGenericType = IsTheRawGenericType(type);
-                if (isTheRawGenericType) return true;
-                type = type.BaseType;
-            }
+        //     // 检查类型
+        //     while (type != null && type != typeof(object))
+        //     {
+        //         isTheRawGenericType = IsTheRawGenericType(type);
+        //         if (isTheRawGenericType) return true;
+        //         type = type.BaseType;
+        //     }
 
-            return false;
+        //     return false;
 
-            // 判断逻辑
-            bool IsTheRawGenericType(Type type) => generic == (type.IsGenericType ? type.GetGenericTypeDefinition() : type);
-        }
+        //     // 判断逻辑
+        //     bool IsTheRawGenericType(Type type) => generic == (type.IsGenericType ? type.GetGenericTypeDefinition() : type);
+        // }
     }
 }
