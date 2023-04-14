90157e470ded0a785bb7dee34de32bc780d26265
LemonNoCry <773596523@qq.com>
2023-04-14 11:46:44 +0800

    ✨ 添加Seq配置

--- Blog.Core.Extensions/ServiceExtensions/SerilogSetup.cs
+++ Blog.Core.Extensions/ServiceExtensions/SerilogSetup.cs
@@ -5,6 +5,7 @@ using Blog.Core.Serilog.Extensions;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Debugging;
+using Serilog.Events;
 using System;
 using System.IO;
 
@@ -26,8 +27,21 @@ public static class SerilogSetup
             //配置日志库
             .WriteToLogBatching();
 
+        //配置Seq日志中心
+        if (AppSettings.app("Seq", "Enabled").ObjToBool())
+        {
+            var address = AppSettings.app("Seq", "Address");
+            var apiKey = AppSettings.app("Seq", "ApiKey");
+            if (!address.IsNullOrEmpty())
+            {
+                loggerConfiguration =
+                    loggerConfiguration.WriteTo.Seq(address, restrictedToMinimumLevel: LogEventLevel.Verbose,
+                        apiKey: apiKey, eventBodyLimitBytes: 10485760);
+            }
+        }
+
         Log.Logger = loggerConfiguration.CreateLogger();
-        
+
         //Serilog 内部日志
         var file = File.CreateText(LogContextStatic.Combine($"SerilogDebug{DateTime.Now:yyyyMMdd}.txt"));
         SelfLog.Enable(TextWriter.Synchronized(file));
