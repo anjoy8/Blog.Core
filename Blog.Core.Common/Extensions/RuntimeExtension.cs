0ddaa35bed9ba0536688f356c1766972380c8cf4
LemonNoCry <773596523@qq.com>
2023-06-12 10:52:11 +0800

    🎨 优化程序集加载

--- Blog.Core.Common/Extensions/RuntimeExtension.cs
+++ Blog.Core.Common/Extensions/RuntimeExtension.cs
@@ -10,75 +10,77 @@ namespace Blog.Core.Common.Extensions;
 
 public static class RuntimeExtension
 {
-    /// <summary>
-    /// 获取项目程序集，排除所有的系统程序集(Microsoft.***、System.***等)、Nuget下载包
-    /// </summary>
-    /// <returns></returns>
-    public static IList<Assembly> GetAllAssemblies()
-    {
-        var list = new List<Assembly>();
-        var deps = DependencyContext.Default;
-        var libs = deps.CompileLibraries.Where(lib => !lib.Serviceable && lib.Type != "package"); //排除所有的系统程序集、Nuget下载包
-        foreach (var lib in libs)
-        {
-            try
-            {
-                var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(lib.Name));
-                list.Add(assembly);
-            }
-            catch (Exception e)
-            {
-                Log.Debug(e, "GetAllAssemblies Exception:{ex}", e.Message);
-            }
-        }
-        return list;
-    }
+	/// <summary>
+	/// 获取项目程序集，排除所有的系统程序集(Microsoft.***、System.***等)、Nuget下载包
+	/// </summary>
+	/// <returns></returns>
+	public static IList<Assembly> GetAllAssemblies()
+	{
+		var list = new List<Assembly>();
+		var deps = DependencyContext.Default;
+		//只加载项目中的程序集
+		var libs = deps.CompileLibraries.Where(lib => !lib.Serviceable && lib.Type == "project"); //排除所有的系统程序集、Nuget下载包
+		foreach (var lib in libs)
+		{
+			try
+			{
+				var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(lib.Name));
+				list.Add(assembly);
+			}
+			catch (Exception e)
+			{
+				Log.Debug(e, "GetAllAssemblies Exception:{ex}", e.Message);
+			}
+		}
 
-    public static Assembly GetAssembly(string assemblyName)
-    {
-        return GetAllAssemblies().FirstOrDefault(assembly => assembly.FullName.Contains(assemblyName));
-    }
+		return list;
+	}
 
-    public static IList<Type> GetAllTypes()
-    {
-        var list = new List<Type>();
-        foreach (var assembly in GetAllAssemblies())
-        {
-            var typeInfos = assembly.DefinedTypes;
-            foreach (var typeInfo in typeInfos)
-            {
-                list.Add(typeInfo.AsType());
-            }
-        }
+	public static Assembly GetAssembly(string assemblyName)
+	{
+		return GetAllAssemblies().FirstOrDefault(assembly => assembly.FullName.Contains(assemblyName));
+	}
 
-        return list;
-    }
+	public static IList<Type> GetAllTypes()
+	{
+		var list = new List<Type>();
+		foreach (var assembly in GetAllAssemblies())
+		{
+			var typeInfos = assembly.DefinedTypes;
+			foreach (var typeInfo in typeInfos)
+			{
+				list.Add(typeInfo.AsType());
+			}
+		}
 
-    public static IList<Type> GetTypesByAssembly(string assemblyName)
-    {
-        var list = new List<Type>();
-        var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(assemblyName));
-        var typeInfos = assembly.DefinedTypes;
-        foreach (var typeInfo in typeInfos)
-        {
-            list.Add(typeInfo.AsType());
-        }
+		return list;
+	}
 
-        return list;
-    }
+	public static IList<Type> GetTypesByAssembly(string assemblyName)
+	{
+		var list = new List<Type>();
+		var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(new AssemblyName(assemblyName));
+		var typeInfos = assembly.DefinedTypes;
+		foreach (var typeInfo in typeInfos)
+		{
+			list.Add(typeInfo.AsType());
+		}
 
-    public static Type GetImplementType(string typeName, Type baseInterfaceType)
-    {
-        return GetAllTypes().FirstOrDefault(t =>
-        {
-            if (t.Name == typeName &&
-                t.GetTypeInfo().GetInterfaces().Any(b => b.Name == baseInterfaceType.Name))
-            {
-                var typeInfo = t.GetTypeInfo();
-                return typeInfo.IsClass && !typeInfo.IsAbstract && !typeInfo.IsGenericType;
-            }
+		return list;
+	}
 
-            return false;
-        });
-    }
+	public static Type GetImplementType(string typeName, Type baseInterfaceType)
+	{
+		return GetAllTypes().FirstOrDefault(t =>
+		{
+			if (t.Name == typeName &&
+				t.GetTypeInfo().GetInterfaces().Any(b => b.Name == baseInterfaceType.Name))
+			{
+				var typeInfo = t.GetTypeInfo();
+				return typeInfo.IsClass && !typeInfo.IsAbstract && !typeInfo.IsGenericType;
+			}
+
+			return false;
+		});
+	}
 }
