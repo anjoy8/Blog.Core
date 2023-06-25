990dcfd756d520600501ca9deba0878d36c4fd72
anjoy8 <3143422472@qq.com>
2023-06-25 17:01:14 +0800

    Update ExceptionHandlerMiddleware.cs

--- Blog.Core.Extensions/Middlewares/ExceptionHandlerMiddleware.cs
+++ Blog.Core.Extensions/Middlewares/ExceptionHandlerMiddleware.cs
@@ -4,56 +4,55 @@ using System.Threading.Tasks;
 using Blog.Core.Model;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
-using Serilog;
 
 namespace Blog.Core.Extensions.Middlewares
 {
-	public class ExceptionHandlerMiddleware
-	{
-		private readonly RequestDelegate _next;
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
 
-		public ExceptionHandlerMiddleware(RequestDelegate next)
-		{
-			_next = next;
-		}
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
 
-		public async Task Invoke(HttpContext context)
-		{
-			try
-			{
-				await _next(context);
-			}
-			catch (Exception ex)
-			{
-				await HandleExceptionAsync(context, ex);
-			}
-		}
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
 
-		private async Task HandleExceptionAsync(HttpContext context, Exception e)
-		{
-			if (e == null) return;
+        private async Task HandleExceptionAsync(HttpContext context, Exception e)
+        {
+            if (e == null) return;
 
-			await WriteExceptionAsync(context, e).ConfigureAwait(false);
-		}
+            await WriteExceptionAsync(context, e).ConfigureAwait(false);
+        }
 
-		private static async Task WriteExceptionAsync(HttpContext context, Exception e)
-		{
-			var message = e.Message;
-			switch (e)
-			{
-				case UnauthorizedAccessException:
-					context.Response.StatusCode = (int) HttpStatusCode.Unauthorized;
-					break;
-				default:
-					context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
-					break;
-			}
+        private static async Task WriteExceptionAsync(HttpContext context, Exception e)
+        {
+            var message = e.Message;
+            switch (e)
+            {
+                case UnauthorizedAccessException:
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    break;
+                default:
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
 
-			context.Response.ContentType = "application/json";
+            context.Response.ContentType = "application/json";
 
-			await context.Response
-				.WriteAsync(JsonConvert.SerializeObject(new ApiResponse(StatusCode.CODE500, message).MessageModel))
-				.ConfigureAwait(false);
-		}
-	}
+            await context.Response
+                .WriteAsync(JsonConvert.SerializeObject(new ApiResponse(StatusCode.CODE500, message).MessageModel))
+                .ConfigureAwait(false);
+        }
+    }
 }
