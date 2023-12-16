3eb331673e2a27533904c7b8ee50a4fd8de511a4
anjoy8 <3143422472@qq.com>
2023-12-16 19:51:39 +0800

    Update AutofacPropertityModuleReg.cs

--- Blog.Core.Api/Filter/AutofacPropertityModuleReg.cs
+++ Blog.Core.Api/Filter/AutofacPropertityModuleReg.cs
@@ -7,6 +7,8 @@ namespace Blog.Core.Extensions
     {
         protected override void Load(ContainerBuilder builder)
         {
+            // 记得要启动服务注册
+            // builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, ServiceBasedControllerActivator>());
             var controllerBaseType = typeof(ControllerBase);
             builder.RegisterAssemblyTypes(typeof(Program).Assembly)
                 .Where(t => controllerBaseType.IsAssignableFrom(t) && t != controllerBaseType)
