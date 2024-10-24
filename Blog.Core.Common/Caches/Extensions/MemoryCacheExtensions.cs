f3641bde9bacdeeb46bdd0063105bc9dcc56914b
LemonNoCry <ningmengbuku@outlook.com>
2024-10-24 23:16:40 +0800

    ✨🎨优化Microsoft.Extensions.Caching.Memory 8 fixes: #373,fixes: #371

--- Blog.Core.Common/Caches/Extensions/MemoryCacheExtensions.cs
+++ Blog.Core.Common/Caches/Extensions/MemoryCacheExtensions.cs
@@ -9,27 +9,48 @@ public static class MemoryCacheExtensions
 {
     #region Microsoft.Extensions.Caching.Memory_6_OR_OLDER
 
-    private static readonly Lazy<Func<MemoryCache, object>> GetEntries6 = new(() => (Func<MemoryCache, object>)Delegate.CreateDelegate(
-        typeof(Func<MemoryCache, object>),
-        typeof(MemoryCache).GetProperty("EntriesCollection", BindingFlags.NonPublic | BindingFlags.Instance).GetGetMethod(true),
-        throwOnBindFailure: true));
+    private static readonly Lazy<Func<MemoryCache, object>> GetEntries6 = new(() =>
+        (Func<MemoryCache, object>)Delegate.CreateDelegate(typeof(Func<MemoryCache, object>),
+            typeof(MemoryCache).GetProperty("EntriesCollection", BindingFlags.NonPublic | BindingFlags.Instance)
+              ?.GetGetMethod(true) ?? throw new InvalidOperationException("Cannot find property 'EntriesCollection' on MemoryCache."),
+            throwOnBindFailure: true));
 
     #endregion
 
     #region Microsoft.Extensions.Caching.Memory_7_OR_NEWER
 
-    private static readonly Lazy<Func<MemoryCache, object>> GetCoherentState = new(() =>
-            CreateGetter<MemoryCache, object>(typeof(MemoryCache)
-               .GetField("_coherentState", BindingFlags.NonPublic | BindingFlags.Instance)));
+    private static readonly Lazy<Func<MemoryCache, object>> GetCoherentState7 = new(() =>
+        CreateGetter<MemoryCache, object>(typeof(MemoryCache)
+           .GetField("_coherentState", BindingFlags.NonPublic | BindingFlags.Instance)));
 
     private static readonly Lazy<Func<object, IDictionary>> GetEntries7 = new(() =>
-            CreateGetter<object, IDictionary>(typeof(MemoryCache)
-               .GetNestedType("CoherentState", BindingFlags.NonPublic)
-               .GetField("_entries", BindingFlags.NonPublic | BindingFlags.Instance)));
+        CreateGetter<object, IDictionary>(typeof(MemoryCache)
+           .GetNestedType("CoherentState", BindingFlags.NonPublic)?
+           .GetField("_entries", BindingFlags.NonPublic | BindingFlags.Instance)));
+
+    #endregion
+
+    #region Microsoft.Extensions.Caching.Memory_8_OR_NEWER
+
+    private static readonly Lazy<Func<MemoryCache, object>> GetCoherentState8 = new(() =>
+        CreateGetter<MemoryCache, object>(typeof(MemoryCache)
+           .GetField("_coherentState", BindingFlags.NonPublic | BindingFlags.Instance)));
+
+    private static readonly Lazy<Func<object, IDictionary>> GetEntries8 = new(() =>
+        CreateGetter<object, IDictionary>(typeof(MemoryCache)
+           .GetNestedType("CoherentState", BindingFlags.NonPublic)?
+           .GetField("_stringEntries", BindingFlags.NonPublic | BindingFlags.Instance)));
+
+    #endregion
 
     private static Func<TParam, TReturn> CreateGetter<TParam, TReturn>(FieldInfo field)
     {
-        var methodName = $"{field.ReflectedType.FullName}.get_{field.Name}";
+        if (field == null)
+        {
+            throw new ArgumentNullException(nameof(field), "Field cannot be null.");
+        }
+
+        var methodName = $"{field.ReflectedType!.FullName}.get_{field.Name}";
         var method = new DynamicMethod(methodName, typeof(TReturn), new[] { typeof(TParam) }, typeof(TParam), true);
         var ilGen = method.GetILGenerator();
         ilGen.Emit(OpCodes.Ldarg_0);
@@ -38,12 +59,13 @@ public static class MemoryCacheExtensions
         return (Func<TParam, TReturn>)method.CreateDelegate(typeof(Func<TParam, TReturn>));
     }
 
-    #endregion
-
     private static readonly Func<MemoryCache, IDictionary> GetEntries =
-        Assembly.GetAssembly(typeof(MemoryCache)).GetName().Version.Major < 7
-            ? (Func<MemoryCache, IDictionary>)(cache => (IDictionary)GetEntries6.Value(cache))
-            : cache => GetEntries7.Value(GetCoherentState.Value(cache));
+        Assembly.GetAssembly(typeof(MemoryCache))?.GetName().Version?.Major switch
+        {
+            < 7 => cache => (IDictionary)GetEntries6.Value(cache),
+            7   => cache => GetEntries7.Value(GetCoherentState7.Value(cache)),
+            _   => cache => GetEntries8.Value(GetCoherentState8.Value(cache)),
+        };
 
     public static ICollection GetKeys(this IMemoryCache memoryCache) =>
         GetEntries((MemoryCache)memoryCache).Keys;
